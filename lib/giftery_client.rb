e51a242b6ca414a3239f235a14a133fa79cc2af1
Ilhom <madrahimov.ilhom@gmail.com>
2015-11-08 21:52:49 +0500

    fix

--- lib/giftery_client.rb
+++ lib/giftery_client.rb
@@ -2,6 +2,7 @@ require "giftery_client/version"
 require 'digest/md5'
 require "net/https"
 require "uri"
+require 'json'
 require 'giftery_client/base'
 require 'giftery_client/connect_test'
 require 'giftery_client/products'
